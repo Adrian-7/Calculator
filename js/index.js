2b0def93784ad917938cf92049ffcbae74edfe80
Adrian-7 <adriansalari8@gmail.com>
2023-10-01 16:48:54 +0300

    dff

--- js/index.js
+++ js/index.js
@@ -1,34 +1,85 @@
-const randomNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 0;
+const { Scene, Game } = Phaser;
 
-function checkGuess() {
-  const guess = parseInt(document.getElementById("guess").value);
-  attempts++;
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
+};
 
-  if (isNaN(guess) || guess < 1 || guess > 100) {
-    setMessage("Introduceți un număr valid între 1 și 100.");
-    return;
-  }
+const game = new Game(config);
 
-  if (guess === randomNumber) {
-    setMessage(
-      `Felicitări! Ați ghicit numărul ${randomNumber} în ${attempts} încercări.`
-    );
-    document.getElementById("guess").disabled = true;
-  } else {
-    const difference = Math.abs(guess - randomNumber);
-    let message = "";
+let player;
 
-    if (difference <= 10) {
-      message = "Încălzire!";
-    } else {
-      message = "Rece!";
-    }
-
-    setMessage(`${message} Mai încercați!`);
-  }
+function preload() {
+  this.load.image("sky", "sky.png");
+  this.load.image("platform", "platform.png");
+  this.load.spritesheet("dude", "dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
 }
 
-function setMessage(msg) {
-  document.getElementById("message").textContent = msg;
+function create() {
+  this.add.image(400, 300, "sky");
+
+  const platforms = this.physics.add.staticGroup();
+  platforms.create(400, 568, "platform").setScale(2).refreshBody();
+
+  player = this.physics.add.sprite(100, 450, "dude");
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.physics.add.collider(player, platforms);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+}
+
+function update() {
+  const cursors = this.input.keyboard.createCursorKeys();
+
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play("left", true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play("right", true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play("turn");
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
 }
