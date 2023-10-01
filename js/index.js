1acb7c98406f98b7a1dcf0983a29ff8259333eec
Adrian-7 <adriansalari8@gmail.com>
2023-10-01 16:40:28 +0300

    svdvsd

--- js/index.js
+++ js/index.js
@@ -1,20 +1,34 @@
-let displayValue = "";
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 0;
 
-function appendToDisplay(value) {
-  displayValue += value;
-  document.getElementById("display").value = displayValue;
-}
+function checkGuess() {
+  const guess = parseInt(document.getElementById("guess").value);
+  attempts++;
 
-function clearDisplay() {
-  displayValue = "";
-  document.getElementById("display").value = displayValue;
-}
-
-function calculateResult() {
-  try {
-    displayValue = eval(displayValue);
-    document.getElementById("display").value = displayValue;
-  } catch (error) {
-    document.getElementById("display").value = "Error";
+  if (isNaN(guess) || guess < 1 || guess > 100) {
+    setMessage("Introduceți un număr valid între 1 și 100.");
+    return;
   }
+
+  if (guess === randomNumber) {
+    setMessage(
+      `Felicitări! Ați ghicit numărul ${randomNumber} în ${attempts} încercări.`
+    );
+    document.getElementById("guess").disabled = true;
+  } else {
+    const difference = Math.abs(guess - randomNumber);
+    let message = "";
+
+    if (difference <= 10) {
+      message = "Încălzire!";
+    } else {
+      message = "Rece!";
+    }
+
+    setMessage(`${message} Mai încercați!`);
+  }
+}
+
+function setMessage(msg) {
+  document.getElementById("message").textContent = msg;
 }
