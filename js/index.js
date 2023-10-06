65ea3771cbfd4c9f21686dddef0ba7c0b8041c20
Adrian-7 <adriansalari8@gmail.com>
2023-10-06 20:19:01 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,85 +1,20 @@
-const { Scene, Game } = Phaser;
+let displayValue = "";
 
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
-};
-
-const game = new Game(config);
-
-let player;
-
-function preload() {
-  this.load.image("sky", "sky.png");
-  this.load.image("platform", "platform.png");
-  this.load.spritesheet("dude", "dude.png", {
-    frameWidth: 32,
-    frameHeight: 48,
-  });
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
 }
 
-function create() {
-  this.add.image(400, 300, "sky");
-
-  const platforms = this.physics.add.staticGroup();
-  platforms.create(400, 568, "platform").setScale(2).refreshBody();
-
-  player = this.physics.add.sprite(100, 450, "dude");
-
-  player.setBounce(0.2);
-  player.setCollideWorldBounds(true);
-
-  this.physics.add.collider(player, platforms);
-
-  this.anims.create({
-    key: "left",
-    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1,
-  });
-
-  this.anims.create({
-    key: "turn",
-    frames: [{ key: "dude", frame: 4 }],
-    frameRate: 20,
-  });
-
-  this.anims.create({
-    key: "right",
-    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1,
-  });
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
 }
 
-function update() {
-  const cursors = this.input.keyboard.createCursorKeys();
-
-  if (cursors.left.isDown) {
-    player.setVelocityX(-160);
-    player.anims.play("left", true);
-  } else if (cursors.right.isDown) {
-    player.setVelocityX(160);
-    player.anims.play("right", true);
-  } else {
-    player.setVelocityX(0);
-    player.anims.play("turn");
-  }
-
-  if (cursors.up.isDown && player.body.touching.down) {
-    player.setVelocityY(-330);
+function calculateResult() {
+  try {
+    displayValue = eval(displayValue);
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    document.getElementById("display").value = "Error";
   }
 }
