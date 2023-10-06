660fa674da08e430665b0baac5c086d1974f53ff
Adrian-7 <adriansalari8@gmail.com>
2023-10-06 21:20:26 +0300

    finisare update

--- js/index.js
+++ js/index.js
@@ -10,11 +10,28 @@ function clearDisplay() {
   document.getElementById("display").value = displayValue;
 }
 
+function deleteLast() {
+  displayValue = displayValue.slice(0, -1); // Se elimină ultimul caracter
+  document.getElementById("display").value = displayValue;
+}
+
 function calculateResult() {
   try {
     displayValue = eval(displayValue);
     document.getElementById("display").value = displayValue;
   } catch (error) {
     document.getElementById("display").value = "Error";
+  }
+}
+
+function calculateResult() {
+  try {
+    // Înlocuiți simbolul "^" cu "**" pentru a efectua calculul la putere
+    displayValue = displayValue.replace(/\^/g, "**");
+    // Evaluați expresia actualizată
+    displayValue = eval(displayValue);
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    document.getElementById("display").value = "Eroare";
   }
 }
